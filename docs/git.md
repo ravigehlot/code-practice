a046405d14bfabcf9cbbec12787ed93e72e46ded
Ravi Gehlot <ravi@gehlot.net>
2021-10-08 16:50:14 -0400

    tweaked eslint to enable jest definitions, add dev to package and cleaned up gridblocks

---
+++ docs/git.md
@@ -0,0 +1,5 @@
+## Git
+
+- Download objects and refs from another repository
+  - git fetch
+  - vscode git integration does it automatically.
--- tests/GridBlocks.test.js
+++ tests/GridBlocks.test.js
@@ -9,14 +9,3 @@ const instance = new GridBlocks(totalNumberofBlocks)
 test('Object instance is of type GridBlocks', () => {
   expect(instance).toBeInstanceOf(GridBlocks)
 })
-
-test('GridBlocks method addBlocks add blocks', () => {
-  document.body.innerHTML = `
-  <div class="grid grid-cols-3 gap-4">
-    <div class="bg-gray-300 h-12 rounded"></div>
-  </div>`
-
-  console.log(document.body.innnerHTML)
-
-  instance.addBlocks()
-})
