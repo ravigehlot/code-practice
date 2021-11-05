2b8099a9bd112e7a61236c95cf04de093c23081c
Ravi Gehlot <ravi@gehlot.net>
2021-11-05 16:53:07 -0400

    remove module stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './css/styles.css'
-import fetchAPI from './js/fetchAPI'
 import GridBlocks from './js/components/GridBlocks'
 
 const gridBlocks = new GridBlocks(9)
--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch'
-
 let fetchAPI = () => {
   const headers = [
     ['Host', process.env.CITYOFNEWYORKUS_HOST],
